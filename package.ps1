e5baa010f2db068845a3321aeff0d1ee34db0b22
Jim Gomes <jgomes@apache.org>
2011-03-08 19:59:21 +0000

    Create 1.5.0 tagged branch of Apache.NMS.EMS. Create 1.5.0 tagged branch of Apache.NMS.MSMQ. Create 1.5.0 tagged branch of Apache.NMS.WCF.

--- package.ps1
+++ package.ps1
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 $pkgname = "Apache.NMS.MSMQ"
-$pkgver = "1.5-SNAPSHOT"
+$pkgver = "1.5.0"
 $configurations = "release", "debug"
 $frameworks = "net-2.0", "net-3.5", "net-4.0"
 
